a28c7e2c10ed2c5667e585db7825f00f70354334
Forpee <khalilhassam3@gmail.com>
2024-08-19 08:40:42 +0200

    chore: cleanup more tests

--- src/run/tests.rs
+++ src/run/tests.rs
@@ -34,26 +34,3 @@ fn test_zk_ads() -> anyhow::Result<()> {
   let result = proof.verify(public_values, &pp)?;
   Ok(assert!(result))
 }
-
-#[test]
-fn test_gradient_boosting() -> anyhow::Result<()> {
-  init_logger();
-
-  // Configure the arguments needed for WASM execution
-  //
-  // Here we are configuring the path to the WASM file
-  let args = WASMArgsBuilder::default()
-    .file_path(PathBuf::from("wasm/gradient_boosting.wasm"))
-    .invoke(Some(String::from("_start")))
-    .build();
-
-  let pp = BatchedZKEngine::setup(&mut WASMCtx::new_from_file(&args)?)?;
-
-  // Create a WASM execution context for proving.
-  let mut wasm_ctx = WASMCtx::new_from_file(&args)?;
-
-  let (proof, public_values, _) = BatchedZKEngine::prove_wasm(&mut wasm_ctx, &pp)?;
-
-  let result = proof.verify(public_values, &pp)?;
-  Ok(assert!(result))
-}
