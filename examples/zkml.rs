6132d58f25fc9df964bd4390e6a2e7c090c01173
Forpee <khalilhassam3@gmail.com>
2024-08-27 14:34:01 +0200

    feat: add zkml example

---
+++ examples/zkml.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+use zk_engine::{
+  args::{WASMArgsBuilder, WASMCtx},
+  traits::zkvm::ZKVM,
+  utils::logging::init_logger,
+  BatchedZKEngine,
+};
+
+fn main() -> anyhow::Result<()> {
+  init_logger();
+
+  let args = WASMArgsBuilder::default()
+    .file_path(PathBuf::from("wasm/gradient_boosting.wasm"))
+    .invoke(Some(String::from("_start")))
+    .build();
+
+  // Create a WASM execution context for proving.
+  let pp = BatchedZKEngine::setup(&mut WASMCtx::new_from_file(&args)?)?;
+
+  let mut wasm_ctx = WASMCtx::new_from_file(&args)?;
+
+  let (proof, public_values, _) = BatchedZKEngine::prove_wasm(&mut wasm_ctx, &pp)?;
+
+  let result = proof.verify(public_values, &pp)?;
+  Ok(assert!(result))
+}
