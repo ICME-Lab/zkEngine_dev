41137d9838c044baa780ab6091de22abaf572736
Wyatt <wyatt@icme.io>
2024-09-10 17:52:50 -0500

    remove time

--- src/circuits/supernova/etable_rom.rs
+++ src/circuits/supernova/etable_rom.rs
@@ -34,7 +34,7 @@ use super::step_circuits::{
   parametric_ops::SelectCircuit,
 };
 
-use std::time::Instant;
+//use std::time::Instant;
 
 /// The Etable ROM to constraint the sequence of execution order for opcode, in the Execution table
 pub struct EtableROM<E1> {
@@ -368,7 +368,7 @@ where
   E1: CurveCycleEquipped,
   <E1 as Engine>::Scalar: PartialOrd,
 {
-  let total_time = Instant::now();
+  //let total_time = Instant::now();
   let mut test_rom = EtableROM::<E1>::new(rom, tracer_values.to_vec());
 
   let pp = PublicParams::setup(&test_rom, &*default_ck_hint(), &*default_ck_hint());
@@ -383,7 +383,7 @@ where
   let last_index = test_rom.rom.len() - 1;
   tracing::info!("Starting NIVC");
 
-  let time = Instant::now();
+  //let time = Instant::now();
   for (i, &op_code) in test_rom.rom.iter().enumerate() {
     if i % 10 == 0 && should_stop() {
       return Err(ProvingError::Interrupted);
@@ -418,10 +418,10 @@ where
     test_rom.counter += 1;
     recursive_snark_option = Some(recursive_snark)
   }
-  tracing::info!("NIVC done in {:?}", time.elapsed());
-  let total_elapsed_time = total_time.elapsed();
+  //tracing::info!("NIVC done in {:?}", time.elapsed());
+  //let total_elapsed_time = total_time.elapsed();
 
-  tracing::info!("The full run took {:?}", total_elapsed_time);
+  //tracing::info!("The full run took {:?}", total_elapsed_time);
 
   assert!(recursive_snark_option.is_some());
   Ok(())
