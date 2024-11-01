8551ae121a690c51466cd86b706d2aa28f6f5f69
Forpee <khalilhassam3@gmail.com>
2024-11-01 11:38:46 +0200

    feat: add in custom errors

--- src/v1/error.rs
+++ src/v1/error.rs
@@ -1,1 +1,18 @@
 //! This module defines errors returned by the library.
+
+use nova::errors::NovaError;
+use thiserror::Error;
+
+/// Errors returned by Spark compiler
+#[derive(Debug, Error)]
+pub enum ZKWASMError {
+  /// Nova error
+  #[error("NovaError")]
+  NovaError(#[from] NovaError),
+  /// returned RecursiveSNARK is badly formed
+  #[error("MalformedRS")]
+  MalformedRS,
+  /// Anyhow Error
+  #[error("AnyhowError")]
+  AnyhowError(#[from] anyhow::Error),
+}
--- src/v1/tests.rs
+++ src/v1/tests.rs
@@ -1,2 +1,16 @@
+use std::path::PathBuf;
+
+use crate::{traits::wasm::ZKWASMArgs, wasm::args::WASMArgsBuilder};
+
+use super::error::ZKWASMError;
+
 #[test]
-fn test_tracing() {}
+fn test_tracing() -> Result<(), ZKWASMError> {
+  let args = WASMArgsBuilder::default()
+    .file_path(PathBuf::from("wasm/nebula/ams_isa.wat"))
+    .build()
+    .bytecode()
+    .map_err(ZKWASMError::AnyhowError)?;
+
+  Ok(())
+}
