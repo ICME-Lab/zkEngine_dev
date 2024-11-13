819a65789e24e390f5ed17270f721b3376a77904
Forpee <khalilhassam3@gmail.com>
2024-11-13 21:07:16 +0200

    chore: update doc comment

--- src/v1/utils/tracing.rs
+++ src/v1/utils/tracing.rs
@@ -1,12 +1,13 @@
-use crate::v1::{
-  error::ZKWASMError,
-  wasm_ctx::{WASMCtx, ZKWASMCtx},
-};
+use crate::v1::{error::ZKWASMError, wasm_ctx::ZKWASMCtx};
 use std::{cell::RefCell, rc::Rc, time::Instant};
 
 use super::macros::{start_timer, stop_timer};
 
 /// Get inner value of [`Rc<RefCell<T>>`]
+///
+/// # Panics
+///
+/// Panics if [`Rc`] is not the sole owner of the underlying data,
 pub fn unwrap_rc_refcell<T>(last_elem: Rc<RefCell<T>>) -> T {
   let inner: RefCell<T> = Rc::try_unwrap(last_elem)
     .unwrap_or_else(|_| panic!("The last_elem was shared, failed to unwrap"));
